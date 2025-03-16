f895f5df066a4062c88bb580b7dd9e7f0f43d78e
Max Fernandes de Souza <xaamax@gmail.com>
2025-03-16 20:46:05 -0300

    Realizando tratamento de erros - Filter Exception

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MyExceptionFilter } from './common/filters/myexception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // Habilita a validação de dados (DTO)
+  app.useGlobalFilters(new MyExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
