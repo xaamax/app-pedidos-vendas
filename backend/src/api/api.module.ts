5c9d2f54a4e453c27d92a0e1fb0156e7298cb7d9
Max Fernandes de Souza <xaamax@gmail.com>
2025-03-16 20:23:03 -0300

    Implementando controle de autenticação

--- backend/src/api/api.module.ts
+++ backend/src/api/api.module.ts
@@ -1,11 +1,19 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
 
-import { ApiController } from "./api.controller";
-import { ApiService } from "./api.service";
+import { ApiController } from './api.controller';
+import { ApiService } from './api.service';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from 'src/common/guards/auth.guard';
 
 @Module({
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [
+    ApiService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
   exports: [ApiService],
 })
 export class ApiModule {}
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ApiModule } from './api/api.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ApiModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ApiModule],
   controllers: [],
   providers: [],
 })
