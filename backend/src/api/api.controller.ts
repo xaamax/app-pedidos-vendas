2c4628aad1368a14d0bf6b54545fc2b8677806ed
Max Fernandes de Souza <xaamax@gmail.com>
2025-03-16 19:54:42 -0300

    Iniciando API

---
+++ backend/src/api/api.controller.ts
@@ -0,0 +1,16 @@
+import {
+  Controller,
+  Get,
+} from '@nestjs/common';
+
+import { ApiService } from './api.service';
+
+@Controller()
+export class ApiController {
+  constructor(private readonly apiService: ApiService) {}
+  
+  @Get('/health')
+  healthCheck() {
+    return this.apiService.healthCheck();
+  }
+}
